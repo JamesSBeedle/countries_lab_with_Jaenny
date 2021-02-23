fc4f0f173b2fe8c900e430beaba4b68d56511ae5
JamesSBeedle <jamessbeedle@gmail.com>
2021-02-23 16:27:17 +0000

    committing to fork to jenny

---
+++ src/components/FavouriteListItem.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div id="favourite_countries">
+    <h2>Favourite Countries</h2>
+    <ul>
+      <li v-for="country in favouriteCountry">
+        <p>Country: {{country.name}}</p>
+        <p>Population: {{country.population}}</p>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'favourite-countries',
+  props: ['favouriteCountry']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/NeighbourCountry.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div id="neighbouring_countries">
+  <h3>{{country.name}}</h3>
+    <ul id="neighbouring_countries">
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'neighbouring-countries',
+  props: []
+}
+</script>
+
+<style lang="css" scoped>
+
+.med-flag {
+  height: 40px
+}
+</style>
